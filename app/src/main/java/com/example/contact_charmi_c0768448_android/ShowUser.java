87a87a7a07e266deb3cac10d3440591103db651b
Charmi <c0768448@mylambton.ca>
2020-07-15 20:10:04 -0400

    add cell layout to edit or delete user

---
+++ app/src/main/java/com/example/contact_charmi_c0768448_android/ShowUser.java
@@ -0,0 +1,14 @@
+package com.example.contact_charmi_c0768448_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_user);
+    }
+}
