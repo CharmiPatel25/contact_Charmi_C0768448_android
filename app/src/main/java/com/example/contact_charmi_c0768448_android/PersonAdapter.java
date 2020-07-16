62739b7555cdd089b053d9d15bcce4697133b493
Charmi <c0768448@mylambton.ca>
2020-07-15 20:19:15 -0400

    add SQLite class

---
+++ app/src/main/java/com/example/contact_charmi_c0768448_android/PersonAdapter.java
@@ -0,0 +1,11 @@
+package com.example.contact_charmi_c0768448_android;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonAdapter {
+
+}
